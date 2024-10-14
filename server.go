66c5e73b332c03c0fee85714c4bc319eecb559ec
Tharinda Wijesekera <seth.tharinda@gmail.com>
2024-10-14 17:14:59 +0530

    feat: create network peers, and connect tcp connections with dial, and create peers to connect with the network, create peer module with support to add messages.

--- server.go
+++ server.go
@@ -4,16 +4,21 @@ import (
 	"dfs/p2p"
 	"fmt"
 	"log"
+	"sync"
 )
 
 type FileServerOpts struct {
 	StorageRoot       string
 	PathTransformFunc p2p.PathTransformFunc
 	Transport         p2p.Transport
+	BootStrapNodes    []string
 }
 
 type FileServer struct {
 	FileServerOpts
+
+	peerLock sync.Mutex
+	peers    map[string]p2p.Peer
 
 	store  *p2p.Store
 	quitch chan struct{}
@@ -28,10 +33,20 @@ func NewFileServer(opts FileServerOpts) *FileServer {
 		store:          p2p.NewStore(storeOpts),
 		FileServerOpts: opts,
 		quitch:         make(chan struct{}),
+		peers:          make(map[string]p2p.Peer),
 	}
 }
 func (s *FileServer) Stop() {
 	close(s.quitch)
+}
+
+func (s *FileServer) OnPeer(p p2p.Peer) error {
+	s.peerLock.Lock()
+	defer s.peerLock.Unlock()
+
+	s.peers[p.RemoteAddr().String()] = p
+	log.Printf("connected with remote: %s", p.RemoteAddr())
+	return nil
 }
 
 func (s *FileServer) loop() {
@@ -50,12 +65,28 @@ func (s *FileServer) loop() {
 	}
 }
 
+func (s *FileServer) bootStrapNetwork() error {
+	for _, addr := range s.BootStrapNodes {
+		if len(addr) == 0 {
+			continue
+		}
+		go func(addr string) {
+			fmt.Println("attempting to connect with remote: ", addr)
+			if err := s.Transport.Dial(addr); err != nil {
+				log.Println("dial error: ", err)
+			}
+		}(addr)
+	}
+
+	return nil
+}
+
 func (s *FileServer) Start() error {
 	if err := s.Transport.ListenAndAccept(); err != nil {
 		return err
 	}
 
+	s.bootStrapNetwork()
 	s.loop()
-
 	return nil
 }
